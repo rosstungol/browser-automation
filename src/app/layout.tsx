1a12d2ad82f663eaddc575c97dd778baa6717a75
Ross Tungol <rossjstungol@gmail.com>
2026-07-29 03:35:16 +0800

    feat: implement clerk auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,17 @@
+import {
+	ClerkProvider,
+	Show,
+	SignInButton,
+	SignUpButton,
+	UserButton,
+} from '@clerk/nextjs'
+import { shadcn } from '@clerk/ui/themes'
 import { Figtree, Geist_Mono } from 'next/font/google'
 
 import './globals.css'
 import { ThemeProvider } from '@/components/theme-provider'
+import { Button } from '@/components/ui/button'
+import { Toaster } from '@/components/ui/sonner'
 import { cn } from '@/lib/utils'
 
 const figtree = Figtree({ subsets: ['latin'], variable: '--font-sans' })
@@ -28,7 +38,29 @@ export default function RootLayout({
 			)}
 		>
 			<body>
-				<ThemeProvider>{children}</ThemeProvider>
+				<ClerkProvider appearance={{ theme: shadcn }}>
+					<header className='flex justify-end gap-2 p-4'>
+						<Show when='signed-out'>
+							<SignInButton mode='modal'>
+								<Button variant='ghost' size='sm'>
+									Sign in
+								</Button>
+							</SignInButton>
+							<SignUpButton mode='modal'>
+								<Button variant='ghost' size='sm'>
+									Sign up
+								</Button>
+							</SignUpButton>
+						</Show>
+						<Show when='signed-in'>
+							<UserButton />
+						</Show>
+					</header>
+					<ThemeProvider>
+						{children}
+						<Toaster />
+					</ThemeProvider>
+				</ClerkProvider>
 			</body>
 		</html>
 	)
