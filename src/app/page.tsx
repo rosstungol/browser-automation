bde238df6f818600d9f28f936d089db7904ad5a8
Ross Tungol <rossjstungol@gmail.com>
2026-07-29 23:21:23 +0800

    feat: enable clerk organizations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
-import { UserButton } from '@clerk/nextjs'
+import { OrganizationSwitcher, UserButton } from '@clerk/nextjs'
 import { auth } from '@clerk/nextjs/server'
 
 export default async function Page() {
 	await auth.protect()
 
-	return <UserButton />
+	return (
+		<div className='flex gap-4 p-8'>
+			<UserButton />
+			<OrganizationSwitcher />
+		</div>
+	)
 }
